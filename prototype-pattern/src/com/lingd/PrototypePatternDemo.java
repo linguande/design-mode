5e602d2135c7b6b76bef35b6c9387c963f36f17a
linguande <13570920879@163.com>
2018-05-10 09:09:09 +0800

    prototype pattern demo

--- prototype-pattern/src/com/lingd/PrototypePatternDemo.java
+++ prototype-pattern/src/com/lingd/PrototypePatternDemo.java
@@ -13,13 +13,13 @@ public class PrototypePatternDemo {
     public static void main(String[] args) {
         ShapeCache.loadCache();
 
-        Shape cloneShape = (Shape) ShapeCache.getShape("1");
+        Shape cloneShape = ShapeCache.getShape("1");
         System.out.println("Shape : " + cloneShape.getType());
 
-        Shape cloneShape2 = (Shape) ShapeCache.getShape("2");
+        Shape cloneShape2 = ShapeCache.getShape("2");
         System.out.println("Shape : " + cloneShape2.getType());
 
-        Shape cloneShape3 = (Shape) ShapeCache.getShape("3");
+        Shape cloneShape3 = ShapeCache.getShape("3");
         System.out.println("Shape : " + cloneShape3.getType());
     }
 }
